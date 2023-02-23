fd524552cb501a34c82f745f5064a7a0ccfa295b
Ariel Santos <105682233+oaksaint@users.noreply.github.com>
2023-02-22 22:03:57 -0300

    Submit final version of the Etch-A-Sketch project

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const gridContainer = document.querySelector('#grid');
 
 // Set the size of the grid container
-gridContainer.style.width = '512px';
-gridContainer.style.height = '512px';
+gridContainer.style.width = '500px';
+gridContainer.style.height = '500px';
 
 // Loop through the rows and columns of the grid
 for (let i = 0; i < 16; i++) {
@@ -12,22 +12,25 @@ for (let i = 0; i < 16; i++) {
     square.classList.add('square');
 
     // Set the size and border of the square div element
-    square.style.width = '30px';
-    square.style.height = '30px';
+    square.style.width = 500 / 16 + 'px';
+    square.style.height = 500 / 16 + 'px';
     square.style.outline = 'solid lightgray 1px';
-    square.style.margin = '1px';
     // Add the square div element to the grid container
     gridContainer.appendChild(square);
   }
 }
-const cellBlock = document.querySelectorAll('.square');
 
+// Select an individual cell block
+let cellBlock = document.querySelectorAll('.square');
+
+// Make the cell block black if you hover the mouse over it
 cellBlock.forEach((item) => {
   item.addEventListener('mouseover', function (event) {
     event.target.style.backgroundColor = 'black';
   });
 });
 
+// Clear the board and make all cell blocks white again
 const clear = function () {
   for (let i = 0; i < cellBlock.length; i++) {
     cellBlock[i].style.backgroundColor = 'white';
@@ -40,9 +43,36 @@ clearButton.onclick = function () {
   clear();
 };
 
+// Prints the range slider value below it as it is being adjusted
 const rangeSlider = document.getElementById('range-slider');
 const sliderValue = document.getElementById('slider-value');
 
 rangeSlider.addEventListener('input', () => {
   sliderValue.innerText = rangeSlider.value;
+  clear();
+
+  const removedSquares = document.querySelectorAll('.square');
+  removedSquares.forEach((item) => item.remove());
+
+  // Loop through the rows and columns of the grid, using the values inputted by the user
+  for (let i = 0; i < rangeSlider.value; i++) {
+    for (let j = 0; j < rangeSlider.value; j++) {
+      // Create a new square div element
+      const square = document.createElement('div');
+      square.classList.add('square');
+
+      // Set the size and border of the square div element, using the values inputted by the user
+      square.style.width = 500 / rangeSlider.value - 0.01 + 'px';
+      square.style.height = 500 / rangeSlider.value - 0.01 + 'px';
+      square.style.outline = 'solid lightgray 1px';
+      // Add the square div element to the grid container
+      gridContainer.appendChild(square);
+
+      // Make the cell block black if you hover the mouse over it
+      square.addEventListener('mouseover', function (event) {
+        event.target.style.backgroundColor = 'black';
+      });
+    }
+    cellBlock = document.querySelectorAll('.square');
+  }
 });
